5b38c5b446c6572479b497f16e5a56f6c4191f2c
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-03-31 18:25:34 -0300

    Desestruturando um objeto

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,16 @@ import { Request, Response } from 'express';
 import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
-    const user = createUser('Bruno', 'brunnodesouzabezerra@gmail.com', '123')
+    const user = createUser({
+        name: 'Bruno',
+        email: 'brunnodesouzabezerra@gmail.com',
+        password: '123',
+        techs: [
+            'Python',
+            'Git',
+            {title: 'Python', experience: 80}
+        ]
+    });
+
     return response.json({ message: 'Hello World!' });
 }
--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -2,13 +2,19 @@
  * Para criar: name, email, senha
  */
 
+interface TechObject {
+    title: string;
+    experience: number;
+}
+
 interface CreateUserData {
     name: string;
     email: string;
     password: string;
+    techs: Array<string | TechObject>;
 }
 
-export default function createUser({ name, email, password }: CreateUserData) {
+export default function createUser({ name, email, password, techs }: CreateUserData) {
     const user = {
         name,
         email,
